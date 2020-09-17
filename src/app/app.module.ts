defec6f24e428fda7e3ff8ca5f3c040bdb6fc0b5
rohitroy-github <rohitdevops.roy@gmail.com>
2020-09-17 13:22:53 +0530

    Component Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -17,6 +21,7 @@ import { MenuComponent } from './menu/menu.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatListModule,
     MatToolbarModule,
     FlexLayoutModule
   ],
